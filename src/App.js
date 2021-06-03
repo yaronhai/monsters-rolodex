279dcb849654320e0cf1c6e814c267a9bbeabb98
yaronhai <72712518+yaronhai@users.noreply.github.com>
2021-06-03 20:50:32 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {CardList} from './components/card-list/card-list.component';
-// import {SearchBox} from './components/search-box/search-box.component';
+import {SearchBox} from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -31,7 +31,12 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Monsters</h1>
-        <CardList monsters={this.state.monsters} /> 
+        <SearchBox 
+          placeholder="search monster"
+          handleChange= {this.handleChange}
+        />
+
+        <CardList monsters={filteredMonsters} /> 
       </div>
     )
   }
